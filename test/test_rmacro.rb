06cc07aac8e316b1e26945c5730e70aaf1e9e8d5
BurdetteLamar <burdettelamar@yahoo.com>
2025-12-21 19:12:09 +0000

    More

--- test/test_rmacro.rb
+++ test/test_rmacro.rb
@@ -15,11 +15,22 @@ class TestRmacro < Minitest::Test
     end
   end
 
-  def test_new_with_bad_argument
+  def test_new_with_bad_instream
     TestRmacro.streams do |_, outstream|
       e = assert_raises ArgumentError do
         RMacro.new('foo', outstream)
       end
+      assert_match(/Input/, e.message)
+      assert_match(/lacks methods/, e.message)
+    end
+  end
+
+  def test_new_with_bad_outstream
+    TestRmacro.streams do |instream, _|
+      e = assert_raises ArgumentError do
+        RMacro.new(instream, 'foo')
+      end
+      assert_match(/Output/, e.message)
       assert_match(/lacks methods/, e.message)
     end
   end
